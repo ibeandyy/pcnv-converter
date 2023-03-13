86e78c062512af520de0a75d3e44d36a5985f7c5
ibeandyy <adhyr4@gmail.com>
2023-03-13 15:05:21 -0700

    .

--- src/DiscordClient.ts
+++ src/DiscordClient.ts
@@ -1,8 +1,12 @@
-import { BaseInteraction, Client, REST, Routes } from "discord.js";
-
-import { token, clientId } from "./config.json";
-import { connectDB } from "./database/schema";
-import wait from "wait";
+import {
+  BaseInteraction,
+  ChatInputCommandInteraction,
+  Client,
+  REST,
+  Routes,
+} from "discord.js";
+import { data, execute } from "./commands/get-conversion";
+import { TOKEN, CLIENTID, GUILD } from "./config.json";
 
 export class DiscordClient extends Client {
   constructor() {
@@ -10,18 +14,36 @@ export class DiscordClient extends Client {
     this.listenButtons();
   }
   async initialize() {
-    connectDB();
-
-    await this.login(token);
+    await this.login(TOKEN);
+    this.setCommands();
   }
   listenButtons() {
     this.on("interactionCreate", async (interaction: BaseInteraction) => {
-      if (!interaction.isButton()) return;
-      if (interaction.customId === "babelight") {
+      if (!interaction.isCommand()) return;
+      if (interaction.commandName === "pcnv-conversion") {
         try {
-          await interaction.deferReply({ ephemeral: true });
-        } catch (e) {}
+          await execute(interaction as ChatInputCommandInteraction);
+        } catch (e) {
+          console.log(e);
+        }
       }
     });
+  }
+  setCommands() {
+    let commands = [];
+    const setBozo = data.toJSON();
+
+    commands.push(setBozo);
+    const rest = new REST({ version: "10" }).setToken(TOKEN);
+    rest
+      .put(Routes.applicationGuildCommands(CLIENTID, GUILD), {
+        body: commands,
+      })
+      .then((data: any) =>
+        console.log(
+          `Successfully registered ${data.length} application commands.`
+        )
+      )
+      .catch(console.error);
   }
 }
---
+++ src/commands/get-conversion.ts
@@ -0,0 +1,87 @@
+import { wallets } from "../wallets.json";
+import { amounts } from "../amounts.json";
+import {
+  ChatInputCommandInteraction,
+  Options,
+  SlashCommandBuilder,
+} from "discord.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("pcnv-conversion")
+  .setDescription(
+    "Get the amount of pCNV you will receive from burning your CNV"
+  )
+  .addSubcommand((option) =>
+    option
+      .setName("all-wallets")
+      .setDescription("Get all wallets and their amounts")
+  )
+
+  .addSubcommand((option) =>
+    option
+      .setName("individual-wallet")
+      .setDescription(
+        "Get the amount of pCNV you will receive from burning your CNV"
+      )
+      .addStringOption((option) =>
+        option
+          .setName("wallet-address")
+          .setDescription("The wallet to look up")
+          .setRequired(true)
+      )
+  );
+
+export const execute = async (interaction: ChatInputCommandInteraction) => {
+  const allAmounts = wallets
+    .map((wallet: string, index: number) => {
+      return {
+        wallet,
+        amount: amounts[index],
+      };
+    })
+    .sort((a: any, b: any) => b.amount - a.amount);
+  if (interaction.options.getSubcommand() === "all-wallets") {
+    let response = allAmounts.map(
+      (wallet: any) => `${wallet.wallet} - ${wallet.amount} CNV\n`
+    );
+    //split response into several arrays of 2000 characters
+    let responseArray = [];
+    let responseString = "";
+    for (let i = 0; i < response.length; i++) {
+      if (responseString.length + response[i].length > 2000) {
+        responseArray.push(responseString);
+        responseString = "";
+      }
+      responseString += response[i];
+    }
+    responseArray.push(responseString);
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i == 0)
+        await interaction.reply({
+          content: responseArray[i],
+          ephemeral: true,
+        });
+      else
+        interaction.followUp({
+          content: responseArray[i],
+          ephemeral: true,
+        });
+    }
+  } else {
+    const userAmount = allAmounts.find((wallet: any) => {
+      return (
+        wallet.wallet ===
+        interaction.options.getString("wallet-address", true).toLowerCase()
+      );
+    });
+    if (!userAmount)
+      return interaction.reply({
+        content: "You have no CNV to convert",
+        ephemeral: true,
+      });
+    await interaction.reply({
+      content: `At conversion under the latest Proposal ${userAmount?.wallet} will have ${userAmount?.amount} CNV`,
+      ephemeral: true,
+    });
+  }
+};
